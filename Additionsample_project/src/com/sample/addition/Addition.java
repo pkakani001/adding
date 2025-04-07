3ba9a6f8227d4c81fb5ee6e4f16d6aed1e264962
priyanka kakani <kakanipriyanka402@gmail.com>
2025-04-07 19:28:57 -0400

    committing addition project folder

---
+++ Additionsample_project/src/com/sample/addition/Addition.java
@@ -0,0 +1,11 @@
+package com.sample.addition;
+
+public class Addition {
+	public static void main(String[] args) {
+	int a = 10;
+	int b = 10;
+	int c = a+b;
+	System.out.println("c value is:" +c);
+
+}
+}
