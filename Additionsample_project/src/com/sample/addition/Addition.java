ce53947f9e4a13ae48e090ed7d4d5e822f49b90a
priyanka kakani <kakanipriyanka402@gmail.com>
2025-04-07 22:28:53 -0400

    updated addition.java

--- Additionsample_project/src/com/sample/addition/Addition.java
+++ Additionsample_project/src/com/sample/addition/Addition.java
@@ -4,8 +4,8 @@ public class Addition {
 	public static void main(String[] args) {
 	int a = 10;
 	int b = 10;
-	int c = a+b;
-	System.out.println("c value is:" +c);
+	int sum = a+b;
+	System.out.println("c value is:" +sum);
 
 }
 }
